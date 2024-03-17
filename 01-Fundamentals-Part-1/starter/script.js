4e75eb824e186f22d7c8749872c87d7420af9c92
sundarsgithub <sundar_55555@yahoo.co.in>
2024-03-17 19:17:42 -0400

    commit

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,157 @@
-let js = 'amazing';
-if (js == 'amazing') alert('Java script is FUN!!!');
+/*let js = 'amazing';
+console.log(40 + 50 - 10);
 
-40 + 10;
-console.log(40 + 10);
+console.log('Jonas');
+console.log(49);
+
+let firstName = "Swathi Sundaram";
+let firstNamePerson = 'Sundar';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstNamePerson);
+
+let name = 'Sundaram';
+let personName = 'Test';
+let PI = 3.145;
+
+let myFirstJob = "Unix Admin";
+let currentJob = "Programmer";
+
+console.log(myFirstJob);
+
+let country = "Canada";
+let continent = "North America";
+let population = "43,000,000";
+console.log(country, continent, population)
+*/
+
+// This is variable name subject
+//let age = 10;
+//let secondName = 'Thanushree';
+//let female = true;
+//let children;
+
+//true;
+//console.log(true);
+//console.log(female);
+//console.log(typeof female);
+//console.log(typeof age);
+//console.log(age);
+//console.log(typeof secondName);
+
+/*
+age = 'ten';
+console.log(typeof age);
+console.log(age);
+*/
+
+/*
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2024;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+
+
+/*
+let age = 30;
+console.log(age);
+age = 31;
+console.log(age);
+
+const birthYear = 2109;
+//birthYear = 2000;
+var job = 'programmer';
+job = 'admin';
+*/
+
+/*
+let country = "Canada";
+let continent = "North America";
+let population = 43000000;
+const isIsland = true;
+let language = 'English';
+
+console.log(isIsland);
+console.log(typeof isIsland);
+
+console.log(population);
+console.log(typeof population);
+console.log(country);
+console.log(typeof country);
+console.log(language);
+console.log(typeof language);
+
+const currentYear = 2024;
+const ageSundar = currentYear - 1986;
+const ageSwathi = currentYear - 1992;
+console.log(ageSundar, ageSwathi);
+
+console.log(ageSundar * 2, ageSundar / 2, 2 ** 3);
+
+const firstName = 'Swathi';
+const space = ' ';
+const lastName = 'Sundaram';
+console.log(firstName + space + lastName)
+
+let x = 10 + 5;
+console.log(x);
+x += 10;
+console.log(x);
+x++;
+console.log(x);
+x--;
+console.log(x);
+
+//console.log(ageSundar =< 20);
+const isAgeSundar = ageSundar >= 18;
+console.log(isAgeSundar);
+console.log(currentYear - 1986 > currentYear - 1992);
+*/
+/*
+const now = 2024;
+const ageSundar = now - 1986;
+const ageSwathi = now - 1992;
+//console.log(now - 1986 > now - 1992);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+console.log(ageSundar, ageSwathi);
+const ageAverage = (ageSundar + ageSwathi) / 2
+console.log(ageAverage);
+
+const childName = 'Thanushree';
+console.log('MY CHILD NAME IS ', childName);
+*/
+
+/*
+let country = "Canada";
+let population = 43000000;
+const isIsland = true;
+let language = 'English';
+
+let splitCanadaPopulation = 43000000 / 2
+console.log('If Canada split into 2 halves, each side will have population of', splitCanadaPopulation);
+
+population++
+let add1toPoulation = population;
+console.log('Canada population added by 1', add1toPoulation);
+
+let finlandPopulation = 6000000;
+console.log('Is Canada population more than Finland', population > finlandPopulation);
+
+let avgPopulation = 33000000;
+console.log('Is Canada population less than avgPopulation', population < avgPopulation);
+
+let description = 'Portugal is in Europe, and its 11 million people speak portuguese';
+console.log(description);
+*/

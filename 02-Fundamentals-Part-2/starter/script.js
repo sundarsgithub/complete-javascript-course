2c5c6d86f7b04fedd943db7d43d90197e317e16c
sundarsgithub <sundar_55555@yahoo.co.in>
2024-04-05 10:57:22 -0400

    commit

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -155,7 +155,7 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 checkWinner(scoreDolphins, scoreKoalas)
 
 */
-
+/*
 const calcAverage = function (score1, score2, score3) {
     return ((score1 + score2 + score3) / 3)
 }
@@ -175,3 +175,75 @@ const checkWinner = function () {
 }
 
 checkWinner()
+*/
+
+/*
+const friends = ['Michael', 'Steven', 'Peter']
+console.log(friends)
+
+const years = new Array(1991, 1992, 1993, 1994)
+console.log(years)
+
+console.log(friends[3])
+console.log(friends.length)
+console.log(friends[friends.length - 1])
+
+friends[2] = 'Jay'
+console.log(friends)
+
+const firstName = 'jonas'
+const jonas = [firstName, 'Sched', 2024 - 1986, 'teacher', friends]
+console.log(jonas)
+console.log(jonas[4])
+
+*/
+
+/*
+const calcAge = function (birthYear) {
+    return 2024 - birthYear
+}
+
+const years = [1990, 1967, 2010, 2018]
+
+const age1 = calcAge(years[0])
+const age2 = calcAge(years[1])
+const age3 = calcAge(years[years.length - 1])
+console.log(age1)
+console.log(age2)
+console.log(age3)
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
+console.log(ages)
+
+*/
+/*
+const friends = ['Michael', 'Steven', 'Peter']
+const newLength = friends.push('Jay')
+console.log(friends)
+console.log(newLength)
+
+friends.unshift('John')
+console.log(friends)
+
+const popped = friends.pop()
+console.log(popped)
+console.log(friends)
+
+friends.shift()
+console.log(friends)
+
+console.log(friends.indexOf('Steven'))
+
+console.log(friends.includes('Steven'))
+console.log(friends.includes('Bob'))
+
+friends.push(23)
+console.log(friends)
+console.log(friends.includes('23'))
+
+if (friends.includes(23)) {
+    console.log('Array has Steven')
+}
+
+*/
+
